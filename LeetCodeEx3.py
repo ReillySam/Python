ddb1acc63e85f5a0846a61ec7238561d4df8564e
Sam Reilly <samreilly132@icloud.com>
2020-09-15 14:22:22 +0200

    Update LeetCodeEx3.py

--- LeetCodeEx3.py
+++ LeetCodeEx3.py
@@ -24,3 +24,73 @@ def binary_gap(n):
     return max([len(i) for i in max_gap])
 
 print(binary_gap(1041))
+
+
+# =================================================================================================================== #
+                                           # Exercise 2 - Passing Cars
+'''
+    A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive 
+    cars on a road.
+    
+    Array A contains only 0s and/or 1s:
+    
+    0 represents a car traveling east,
+    1 represents a car traveling west.
+    The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is 
+    traveling to the east and Q is traveling to the west.
+    
+    For example, consider array A such that:
+    
+      A[0] = 0
+      A[1] = 1
+      A[2] = 0
+      A[3] = 1
+      A[4] = 1
+    We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4)
+    The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000
+'''
+
+def solution(a):
+    # count how many elements (0's & 1's) each element has. if 0 check the left, if 1 check right
+    # track movement of 0's and 1's. P must be less than Q to be passing
+    count = 0
+    passes = 0
+    for i in a:
+        if i == 0:
+            count += 1
+        else:
+            passes += count
+        # handle max passing cars
+        if i > 1000000000:
+            return - 1
+    return passes
+
+
+a = [0, 1, 0, 1, 1]
+print(solution(a))
+
+# =================================================================================================================== #
+                                       # Exercise 3 - Find smallest positive integer
+'''
+    You are given an unsorted array with both positive and negative elements. You have to find the smallest positive 
+    number missing from the array in O(n) time using constant extra space. You can modify the original array.
+    
+    Example:
+    Input:  {2, 3, 7, 6, 8, -1, -10, 15}
+    Output: 1
+'''
+
+def smallest_posit_int(a):
+    if len(a) == 0:
+        return 0
+    # sort array to increase O(n)
+    a.sort()
+    smallest = 1
+    for i in a:
+        if smallest in a:
+            smallest += 1
+    return smallest
+
+# a = [2, 3, 7, 6, 8, -1, -10, 15]
+a = [1, 2, 3, 6, 10, 5, 8] # output = 4
+print(smallest_posit_int(a))
