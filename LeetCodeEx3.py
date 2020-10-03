16b7dafd3e89901eba67dc41cd7b1ddcde289863
Sam Reilly <samreilly132@icloud.com>
2020-10-03 12:07:29 +0200

    Update LeetCodeEx3.py

--- LeetCodeEx3.py
+++ LeetCodeEx3.py
@@ -259,3 +259,52 @@ class Solution(object):
             output.append((level_sum / level_nodes))
 
         return output
+
+# =================================================================================================================== #
+                                    # Exercise 7 - Index of dominator in array
+'''
+    An array A consisting of N integers is given. The dominator of array A is the value that occurs in more than 
+    half of the elements of A.
+    
+    For example, consider array A such that
+    
+     A[0] = 3    A[1] = 4    A[2] =  3
+     A[3] = 2    A[4] = 3    A[5] = -1
+     A[6] = 3    A[7] = 3
+    The dominator of A is 3 because it occurs in 5 out of 8 elements of A (namely in those with indices 0, 2, 4, 6 and 7) 
+    and 5 is more than a half of 8.
+    
+    Write a function:
+    def solution(A)
+    
+    that, given an array A consisting of N integers, returns index of any element of array A in which the dominator 
+    of A occurs. The function should return −1 if array A does not have a dominator
+'''
+
+def solution(A):
+    if len(A) > 100000: return -1
+    # first find dominator
+    A.sort()
+    dominator = []
+    counter = 0
+    for i in A:
+        occurrence = A.count(i)
+        if occurrence > counter:
+            counter = occurrence
+            dominator = i
+    if counter <= len(A)/2:
+        return -1
+    # next find index
+    #idx = [A.index(dominator) for i in A]
+    return [i for i,x in enumerate(A) if x == dominator]
+
+
+def solution_2(A):
+    dominator = max(set(A), key = A.count)
+    return [i for i,x in enumerate(A) if x == dominator]
+
+
+# This function is not correct, will fix
+print(solution([3, 4, 3, 2, 3, -1, 3, 3]))
+# Correct, elegant and simple
+print(solution_2([3, 4, 3, 2, 3, -1, 3, 3]))
