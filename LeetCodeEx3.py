5e9d3188c6345fcecbb6ae620efdba79fa2efd7c
Sam Reilly <samreilly132@icloud.com>
2020-10-08 11:56:10 +0200

    Update LeetCodeEx3.py

--- LeetCodeEx3.py
+++ LeetCodeEx3.py
@@ -308,3 +308,99 @@ def solution_2(A):
 print(solution([3, 4, 3, 2, 3, -1, 3, 3]))
 # Correct, elegant and simple
 print(solution_2([3, 4, 3, 2, 3, -1, 3, 3]))
+
+
+# =================================================================================================================== #
+                                    # Exercise 8 - Available captures for Rook
+'''
+    On an 8 x 8 chessboard, there is one white rook.  There also may be empty squares, white bishops, and black pawns.  
+    These are given as characters 'R', '.', 'B', and 'p' respectively. Uppercase characters represent white pieces, and 
+    lowercase characters represent black pieces. The rook moves as in the rules of Chess: it chooses one of four cardinal
+    directions (north, east, west, and south), then moves in that direction until it chooses to stop, reaches the edge of 
+    the board, or captures an opposite colored pawn by moving to the same square it occupies.  Also, rooks cannot move
+    into the same square as other friendly bishops.
+    
+    Return the number of pawns the rook can capture in one move
+    
+    Input: [[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".","R",".",".",".","p"],
+    [".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],
+    [".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]]
+    Output: 3
+'''
+
+
+class Solution(object):
+    def numRookCaptures(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        captures = 0
+        rook_index = (0, 0)
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == 'R':
+                    rook_index = (row, col)
+                    break
+
+        # check left
+        flag = True
+        pawn = 0
+        col = rook_index[1] - 1
+        while col >= 0:
+            if board[rook_index[0]][col] == 'B':
+                flag = False
+                break
+            if board[rook_index[0][col] == 'p':
+            pawn + 1
+                break
+            col -= 1
+        if flag and pawn != 0:
+            captures += 1
+
+        # check right
+        flag = True
+        pawn = 0
+        col = rook_index[1] + 1
+        while col < len(board[0]):
+            if board[rook_index][col] == 'B':
+                flag = False
+                break
+            if board[rook_index][col] == 'p':
+                pawn + 1
+                break
+            col += 1
+        if flag and col != 0:
+            captures += 1
+
+        # check down
+        flag = True
+        pawn = 0
+        row = rook_index[0] + 1
+        while row < len(board):
+            if board[row][rook_index[1]] == 'B':
+                flag = False
+                break
+            if board[rook_index][col[1]] == 'p':
+                pawn += 1
+                break
+            col += 1
+        if flag and col != 0:
+            captures += 1
+
+        # check up
+        flag = True
+        pawn = 0
+        row = rook_index[0] - 1
+        while row >= 0:
+            if board[row][rook_index[1]] == 'B':
+                flag = False
+                break
+            if board[rook_index][col[1]] == 'p':
+                pawn += 1
+                break
+            col += 1
+        if flag and col != 0:
+            captures += 1
+
+        return captures
